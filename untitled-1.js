367e9cf18d60cfa972bb7e2a8c3329f5b9094d4a
Adrian Shepelavey <adrianshepelavey@gmail.com>
2019-04-19 21:35:10 -0400

    build it: adding total selector - test data

--- untitled-1.js
+++ untitled-1.js
@@ -1,2 +1,22 @@
 // Create expenses-total.js and expenses-total.test.js
 import selectExpensesTool from '../../selectors/expenses-total.js';
+
+const expenses = [{
+  id: '1',
+  description: 'Gum',
+  note: '',
+  amount: 195,
+  createdAt: 0
+}, {
+  id: '2',
+  description: 'Rent',
+  note: '',
+  amount: 109500,
+  createdAt: moment(0).subtract(4, 'days').valueOf()
+} , {
+  id: '3',
+  description: 'Credit Card',
+  note: '',
+  amount: 4500,
+  createdAt: moment(0).subtract(4, 'days').valueOf()
+}];
